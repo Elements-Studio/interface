7588ae306f3e44ecaa4b709cc628a85fc953e52f
Paradis Sivers <git@github.com>
2022-01-25 09:56:14 +0800

    Add stake info page with star and tvl info

---
+++ src/components/Stake/TokenHarvestDialog.tsx
@@ -0,0 +1,178 @@
+import { Currency, Token } from '@uniswap/sdk-core'
+import { KeyboardEvent, RefObject, useCallback, useEffect, useMemo, useRef, useState, useContext } from 'react'
+import ReactGA from 'react-ga'
+import { t, Trans } from '@lingui/macro'
+import { FixedSizeList } from 'react-window'
+import { Text } from 'rebass'
+import { TYPE } from '../../theme'
+import styled, { ThemeContext } from 'styled-components'
+import Column, { AutoColumn, ColumnCenter, ColumnRight } from '../Column'
+import Row, { RowBetween, AutoRow } from '../Row'
+import Modal from '../Modal'
+import { ButtonFarm, ButtonBorder, ButtonText } from 'components/Button'
+import { useActiveWeb3React } from 'hooks/web3'
+import { useStarcoinProvider } from 'hooks/useStarcoinProvider'
+import BigNumber from 'bignumber.js'
+import { arrayify, hexlify } from '@ethersproject/bytes'
+import { utils, bcs } from '@starcoin/starcoin'
+
+const Container = styled.div`
+  border-radius: 20px;
+  border: 1px solid ${({ theme }) => theme.inputBorder};
+  width: 100%;
+  height: 88px;
+  margin-top: 16px;
+  display: flex;
+  justify-content: space-between;
+`
+
+const Input = styled.input`
+  color: ${({ theme }) => theme.text1};
+  width: 0;
+  position: relative;
+  font-weight: 500;
+  outline: none;
+  border: none;
+  flex: 1 1 auto;
+  background-color: ${({ theme }) => theme.bg1};
+  font-size: 24px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding: 0px;
+  -webkit-appearance: textfield;
+  text-align: right;
+
+  ::-webkit-search-decoration {
+    -webkit-appearance: none;
+  }
+
+  [type='number'] {
+    -moz-appearance: textfield;
+  }
+
+  ::-webkit-outer-spin-button,
+  ::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+  }
+
+  ::placeholder {
+    color: ${({ theme }) => theme.text4};
+  }
+`
+
+interface FarmHarvestDialogProps {
+  tokenX: any,
+  tokenY: any,
+  tbdEarned: number,
+  tbdScalingFactor: number,
+  isOpen: boolean,
+  onDismiss: () => void
+}
+
+export default function FarmHarvestDialog({
+  tokenX,
+  tokenY,
+  tbdEarned,
+  tbdScalingFactor,
+  onDismiss,
+  isOpen,
+}: FarmHarvestDialogProps) {
+
+  const starcoinProvider = useStarcoinProvider();
+  const { account, chainId } = useActiveWeb3React()
+
+  const theme = useContext(ThemeContext)
+  
+  const [harvestNumber, setHarvestNumber] = useState<any>('')
+
+  function parseHarvestNumber(value: string) {
+    setHarvestNumber(value)
+  }
+
+  async function onClickHarvestConfirm() {
+    try {
+      // const functionId = '0x3db7a2da7444995338a2413b151ee437::TokenSwapFarmScript::harvest';
+      const functionId = '0x4783d08fb16990bd35d83f3e23bf93b8::TokenSwapFarmScript::harvest';
+      const strTypeArgs = [tokenX, tokenY];
+      const structTypeTags = utils.tx.encodeStructTypeTags(strTypeArgs);
+
+      const harvestAmount = new BigNumber(harvestNumber).times('1000000000'); // harvestAmount * 1e9
+
+      const harvestAmountSCSHex = (function () {
+        const se = new bcs.BcsSerializer();
+        se.serializeU128(new BigNumber(harvestAmount).toNumber());
+        return hexlify(se.getBytes());
+      })();
+      const args = [
+        arrayify(harvestAmountSCSHex)
+      ];
+
+      const scriptFunction = utils.tx.encodeScriptFunction(
+        functionId,
+        structTypeTags,
+        args,
+      );
+      const payloadInHex = (function () {
+        const se = new bcs.BcsSerializer();
+        scriptFunction.serialize(se);
+        return hexlify(se.getBytes());
+      })();
+
+      await starcoinProvider.getSigner().sendUncheckedTransaction({
+        data: payloadInHex,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+    return false;
+  }
+ 
+  return (
+    <Modal isOpen={isOpen} onDismiss={onDismiss} dialogBg={ theme.bgCard }>
+      <ColumnCenter style={{ padding: '27px 32px'}}>
+        <AutoRow>
+          <TYPE.black fontWeight={500} fontSize={20}>
+            <Trans>Harvest TBD</Trans>
+          </TYPE.black>
+        </AutoRow>
+        <RowBetween style={{ marginTop: '8px' }}>
+          <TYPE.black fontWeight={500} fontSize={14} style={{ marginTop: '10px', lineHeight: '20px' }}>
+            <Trans>TBD Earned</Trans>：{tbdEarned / tbdScalingFactor}
+          </TYPE.black>
+        </RowBetween>
+        <Container>
+          <Input
+            placeholder={'0.0'}
+            value={harvestNumber}
+            onChange={(e) => parseHarvestNumber(e.target.value)}
+            style={{ height: '28px', background: 'transparent', textAlign: 'left', marginTop: '28px', marginLeft: '18px' }}
+          />
+          <ColumnRight style={{ marginRight: '25px', textAlign: 'right' }}>
+            <ButtonText style={{ marginTop: '28px', lineHeight: '28px' }} onClick={() => { setHarvestNumber((tbdEarned / tbdScalingFactor).toString()) }}>
+              <TYPE.black fontWeight={500} fontSize={20} color={'#FD748D'} style={{ lineHeight: '28px' }}>
+                <Trans>MAX</Trans>
+              </TYPE.black>
+            </ButtonText>
+          </ColumnRight>
+        </Container>
+        <RowBetween style={{ marginTop: '24px' }}>
+          <ButtonBorder marginRight={22} onClick={onDismiss} >
+            <TYPE.black fontSize={20}>
+              <Trans>Cancel</Trans>
+            </TYPE.black>
+          </ButtonBorder>
+          <ButtonFarm onClick={() => {
+            onClickHarvestConfirm();
+            setTimeout(onDismiss, 2500);
+            setTimeout("window.location.reload()", 10000);
+          }}>
+            <TYPE.main color={'#fff'}>
+              <Trans>Confirm</Trans>
+            </TYPE.main>
+          </ButtonFarm>
+        </RowBetween>
+      </ColumnCenter>
+    </Modal>
+  )
+}
---
+++ src/pages/Stake/index.tsx
@@ -0,0 +1,330 @@
+import { Trans } from '@lingui/macro'
+import { useCallback, useContext, useState } from 'react'
+import { ChevronDown, ChevronUp } from 'react-feather'
+import styled, { ThemeContext } from 'styled-components'
+import { RouteComponentProps, Link } from 'react-router-dom'
+import { Text } from 'rebass'
+import QuestionHelper from '../../components/QuestionHelper'
+import Row, { AutoRow, RowFixed, RowBetween } from '../../components/Row'
+import { TYPE,IconWrapper } from '../../theme'
+import { ButtonFarm } from '../../components/Button'
+import { AutoColumn } from '../../components/Column'
+import StakeTitle from '../../components/Stake/StakeTitle'
+import { SwitchLocaleLink } from '../../components/SwitchLocaleLink'
+import FarmCard from '../../components/farm/FarmCard'
+import CurrencyLogo from '../../components/CurrencyLogo'
+import { marginTop, maxWidth, paddingTop } from 'styled-system'
+import EthereumLogo from '../../assets/images/ethereum-logo.png'
+import STCLogo from '../../assets/images/stc.png'
+import STCBlueLogo from '../../assets/images/stc_logo_blue.png'
+import PortisIcon from '../../assets/images/portisIcon.png'
+import { useIsDarkMode } from '../../state/user/hooks'
+import { useActiveWeb3React } from 'hooks/web3'
+
+import axios from 'axios';
+import useSWR from "swr";
+
+export const FixedHeightRow = styled(RowBetween)`
+  height: 30px;
+`
+
+const fetcher = (url:any) => axios.get(url).then(res => res.data)
+
+/*
+const fetcher = (
+  url:any,
+  {
+    method: 'POST', 
+    mode: 'no-cors',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: {"key" : "some text"}
+  }
+  ) => fetch(url).then((res) => res.json());
+*/
+
+const StyledEthereumLogo = styled.img<{ size: string }>`
+  width: ${({ size }) => size};
+  height: ${({ size }) => size};
+  box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.075);
+  border-radius: 4px;
+`
+const FarmRow = styled(RowBetween)`
+  background: ${({ theme }) => theme.bg7};
+  line-height: 20px;
+  border-radius: 8px;
+  padding: 6px 16px;
+`
+
+
+export default function Farm({ history }: RouteComponentProps) {
+  let network = 'barnard';
+  const { account, chainId } = useActiveWeb3React()
+  if (chainId === 1) {
+    network = 'main';
+  }
+
+  /*
+  const { data, error } = useSWR(
+    // `http://k8s-default-starswap-af6ced600d-1022591271.ap-northeast-1.elb.amazonaws.com/${network}/v1/lpTokenFarms`,
+    `https://swap-api.starcoin.org/${network}/v1/lpTokenFarms`,
+    fetcher
+  );
+  */
+  const { data: pool, error } = useSWR(
+    // `http://k8s-default-starswap-af6ced600d-1022591271.ap-northeast-1.elb.amazonaws.com/${network}/v1/lpTokenFarms`,
+    `https://swap-api.starcoin.org/${network}/v1/syrupPools`,
+    fetcher
+  );
+
+  /*
+  const { data: poolInUSD, error: error2 } = useSWR(
+    // `http://k8s-default-starswap-af6ced600d-1022591271.ap-northeast-1.elb.amazonaws.com/${network}/v1/lpTokenFarms`,
+    `https://swap-api.starcoin.org/${network}/v1/syrupPoolsTvlInUsd`,
+    fetcher
+  );
+  */
+  
+  // if (error) return "An error has occurred.";
+  // if (!data) return "Loading...";
+  if (error) return null;
+  if (!pool) return null;
+  const list = pool.filter((item:any)=>item.description==='STAR');
+
+  // const darkMode = useIsDarkMode();
+
+  /*
+  const list = [
+    {
+      liquidityTokenFarmId: { 
+        farmAddress: "1", 
+        liquidityTokenId: { tokenXId: "Bot", tokenYId: "Ddd", liquidityTokenAddress: "0x07fa08a855753f0ff7292fdcbe871216" }
+      },
+      description: "Bot<->Ddd Pool", 
+      sequenceNumber: 11, 
+      totalStakeAmount: null, 
+      deactived: false, 
+      createdBy: "admin", 
+      updatedBy: "admin", 
+      createdAt: 1630661107485, 
+      updatedAt: 1630661107485 
+    },
+    {
+      liquidityTokenFarmId: { 
+        farmAddress: "2", 
+        liquidityTokenId: { tokenXId: "Bot", tokenYId: "Ddd", liquidityTokenAddress: "0x07fa08a855753f0ff7292fdcbe871216" }
+      },
+      description: "Bot<->Ddd Pool", 
+      sequenceNumber: 11, 
+      totalStakeAmount: null, 
+      deactived: false, 
+      createdBy: "admin", 
+      updatedBy: "admin", 
+      createdAt: 1630661107485, 
+      updatedAt: 1630661107485 
+    },
+    {
+      liquidityTokenFarmId: { 
+        farmAddress: "0x07fa08a855753f0ff7292fdcbe871216", 
+        liquidityTokenId: { tokenXId: "Bot", tokenYId: "Ddd", liquidityTokenAddress: "0x07fa08a855753f0ff7292fdcbe871216" }
+      },
+      description: "Bot<->Ddd Pool", 
+      sequenceNumber: 11, 
+      totalStakeAmount: null, 
+      deactived: false, 
+      createdBy: "admin", 
+      updatedBy: "admin", 
+      createdAt: 1630661107485, 
+      updatedAt: 1630661107485 
+    },
+    {
+      liquidityTokenFarmId: { 
+        farmAddress: "3", 
+        liquidityTokenId: { tokenXId: "Bot", tokenYId: "Ddd", liquidityTokenAddress: "0x07fa08a855753f0ff7292fdcbe871216" }
+      },
+      description: "Bot<->Ddd Pool", 
+      sequenceNumber: 11, 
+      totalStakeAmount: null, 
+      deactived: false, 
+      createdBy: "admin", 
+      updatedBy: "admin", 
+      createdAt: 1630661107485, 
+      updatedAt: 1630661107485 
+    },
+    {
+      liquidityTokenFarmId: { 
+        farmAddress: "0x07fa08a855753f0ff7292fdcbe871216", 
+        liquidityTokenId: { tokenXId: "Bot", tokenYId: "Ddd", liquidityTokenAddress: "0x07fa08a855753f0ff7292fdcbe871216" }
+      },
+      description: "Bot<->Ddd Pool", 
+      sequenceNumber: 11, 
+      totalStakeAmount: null, 
+      deactived: false, 
+      createdBy: "admin", 
+      updatedBy: "admin", 
+      createdAt: 1630661107485, 
+      updatedAt: 1630661107485 
+    },
+    {
+      liquidityTokenFarmId: { 
+        farmAddress: "0x07fa08a855753f0ff7292fdcbe871216", 
+        liquidityTokenId: { tokenXId: "Bot", tokenYId: "Ddd", liquidityTokenAddress: "0x07fa08a855753f0ff7292fdcbe871216" }
+      },
+      description: "Bot<->Ddd Pool", 
+      sequenceNumber: 11, 
+      totalStakeAmount: null, 
+      deactived: false, 
+      createdBy: "admin", 
+      updatedBy: "admin", 
+      createdAt: 1630661107485, 
+      updatedAt: 1630661107485 
+    },
+  ]
+  */
+
+  return (
+    <>
+      <StakeTitle />
+      <AutoRow justify="center" style={{ paddingTop: '50px', maxWidth: '1200px' }}>
+        {list ? list.map((item:any,index:any) => (
+            <FarmCard key={index}>
+              <AutoColumn justify="center">
+                <RowFixed>
+                  <StyledEthereumLogo src={STCBlueLogo} size={'48px'} />
+                  {/*
+                  <StyledEthereumLogo src={EthereumLogo} style={{ marginRight: '1.25rem' }} size={'48px'} />
+                  */}
+                </RowFixed>
+                <Text fontSize={16} marginTop={23}>
+                  {item.syrupPoolId.tokenId}
+                </Text>
+              </AutoColumn>
+              <FarmRow style={{ marginTop: '30px' }}>
+                <RowFixed>
+                  <TYPE.black fontWeight={400} fontSize={14}>
+                    <Trans>Token Address</Trans>
+                  </TYPE.black>
+                </RowFixed>
+                <RowFixed>
+                  <TYPE.black fontSize={14}>
+                    {item.syrupPoolId.poolAddress.substring(0,16)+'...'} 
+                  </TYPE.black>
+                </RowFixed>
+              </FarmRow>
+              <FarmRow style={{ marginTop: '10px' }}>
+                <RowFixed>
+                  <TYPE.black fontWeight={400} fontSize={14}>
+                    <Trans>TVL in USDT</Trans>
+                  </TYPE.black>
+                </RowFixed>
+                <RowFixed>
+                  <TYPE.black fontSize={14}>
+                    {item.tvlInUsd || 0}
+                  </TYPE.black>
+                </RowFixed>
+              </FarmRow>
+              <FarmRow style={{ marginTop: '10px', marginBottom: '20px' }}>
+                <RowFixed>
+                  <TYPE.black fontWeight={400} fontSize={14}>
+                    <Trans>Total Stake Amount</Trans>
+                  </TYPE.black>
+                </RowFixed>
+                <RowFixed>
+                  <TYPE.black fontSize={14}>
+                    {item.totalStakeAmount || 0}
+                  </TYPE.black>
+                </RowFixed>
+              </FarmRow>
+              <FarmRow style={{ marginTop: '10px', marginBottom: '20px' }}>
+                <RowFixed>
+                  <TYPE.black fontWeight={400} fontSize={14}>
+                    <Trans>Reward Token</Trans>
+                  </TYPE.black>
+                </RowFixed>
+                <RowFixed>
+                  <TYPE.black fontSize={14}>
+                    {item.rewardTokenId}
+                  </TYPE.black>
+                </RowFixed>
+              </FarmRow>
+              <FarmRow style={{ marginTop: '10px', marginBottom: '20px' }}>
+                <RowFixed>
+                  <TYPE.black fontWeight={400} fontSize={14}>
+                    <Trans>Pool Created</Trans>
+                  </TYPE.black>
+                </RowFixed>
+                <RowFixed>
+                  <TYPE.black fontSize={14}>
+                    {(new Date(item.createdAt)+'').slice(4,24)}
+                  </TYPE.black>
+                </RowFixed>
+              </FarmRow>
+              <FarmRow style={{ marginTop: '10px', marginBottom: '20px' }}>
+                <RowFixed>
+                  <TYPE.black fontWeight={400} fontSize={14}>
+                    <Trans>Pool Updated</Trans>
+                  </TYPE.black>
+                </RowFixed>
+                <RowFixed>
+                  <TYPE.black fontSize={14}>
+                    {(new Date(item.updatedAt)+'').slice(4,24)}
+                  </TYPE.black>
+                </RowFixed>
+              </FarmRow>
+              {/* <FarmRow style={{ marginTop: '10px', background: '#2FD8B2', marginBottom: '30px' }}>
+                <RowFixed>
+                  <TYPE.black fontWeight={400} fontSize={14}>
+                    <Trans>Estimated annualized rate of return:</Trans>       
+                  </TYPE.black>
+                </RowFixed>
+                <RowFixed>
+                  <TYPE.black fontSize={14} style={{wordBreak: 'break-all'}}>
+                    {item.estimatedApy}
+                  </TYPE.black>
+                </RowFixed>
+              </FarmRow> */}
+              <FixedHeightRow>
+                <Text fontSize={16} fontWeight={500}>
+                  <Trans>APR</Trans>
+                </Text>
+                <RowFixed>
+                  <Text fontSize={16} fontWeight={500}>
+                    {item.estimatedApy}%
+                  </Text>
+                  <QuestionHelper
+                    text={
+                      <Trans>The estimated annualized percentage yield of rewards.</Trans>
+                    }
+                  />
+                </RowFixed>
+              </FixedHeightRow>
+              <FixedHeightRow>
+                <Text fontSize={16} fontWeight={500}>
+                  <Trans>Multiplier</Trans>
+                </Text>
+                <RowFixed>
+                  <Text fontSize={16} fontWeight={500}>
+                    {item.rewardMultiplier || 1}
+                  </Text>
+                  <QuestionHelper
+                    text={
+                      <Trans>The Multiplier represents the proportion of STAR rewards each farm receives, as a proportion of the STAR produced each block.<br /><br />
+                      For example, if a 1x farm received 1 STAR per block, a 40x farm would receive 40 STAR per block.<br /><br />
+                      This amount is already included in all APR calculations for the farm.</Trans>
+                    }
+                  />
+                </RowFixed>
+              </FixedHeightRow>
+              <ButtonFarm as={Link} to={`/stake/STAR`}>
+                <TYPE.main color={'#fff'}>
+                  <Trans>Stake</Trans>
+                </TYPE.main>
+              </ButtonFarm>
+            </FarmCard>
+        )) : null}
+      </AutoRow>
+      <SwitchLocaleLink />
+    </>
+  )
+}
