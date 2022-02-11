871f816ffeac210a3f3255b772b7414f1f017f6c
Paradis Sivers <git@github.com>
2022-02-11 10:28:12 +0800

    Add network switch for stake title(tvl in usd)

--- src/components/Stake/StakeTitle.tsx
+++ src/components/Stake/StakeTitle.tsx
@@ -1,6 +1,7 @@
 import { Trans } from '@lingui/macro'
 import { useMemo } from 'react'
 import ReactGA from 'react-ga'
+import { useActiveWeb3React } from 'hooks/web3'
 import styled from 'styled-components/macro'
 import { StyledInternalLink, TYPE } from '../../theme'
 
@@ -27,10 +28,20 @@ const TitleTotal = styled.div<{ margin?: string; maxWidth?: string }>`
 `
 
 export default function FarmTitle() {
+    let network = 'barnard';
+    const { chainId } = useActiveWeb3React()
+    if (chainId === 1) {
+      network = 'main';
+    }
+    if (chainId === 252) {
+      network = 'proxima';
+    }
+
+  console.log({network})
 
     const { data, error } = useSWR(
       // "http://a1277180fcb764735801852ac3de308f-21096515.ap-northeast-1.elb.amazonaws.com:80/v1/starswap/farmingTvlInUsd",
-      "https://swap-api.starcoin.org/barnard/v1/syrupPoolTvlInUsd",
+      `https://swap-api.starcoin.org/${network}/v1/syrupPoolTvlInUsd`,
       fetcher
     );
 
