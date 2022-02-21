5a34e0de8bb7bff2472b7b888501971d5f83ac2d
Paradis Sivers <git@github.com>
2022-02-21 16:48:35 +0800

    Add XUSDT address to Barnard network to fix error on stake page

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -61,6 +61,13 @@ export const STAR: { [chainId: number]: Token } = {
 //   ),
 // }
 export const XUSDT: { [chainId: number]: Token } = {
+  [SupportedChainId.BARNARD]: new Token(
+    SupportedChainId.BARNARD,
+    '0xb6d69dd935edf7f2054acf12eb884df8::XUSDT::XUSDT',
+    9,
+    'XUSDT',
+    'XUSDT'
+  ),
   [SupportedChainId.PROXIMA]: new Token(
     SupportedChainId.PROXIMA,
     '0xb6d69dd935edf7f2054acf12eb884df8::XUSDT::XUSDT',
