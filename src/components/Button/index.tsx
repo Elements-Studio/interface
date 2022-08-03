34677b51efef2dd0089216d4c9858a562c3a8344
nsnhhuewwr <nsnhhuewwr@outlook.com>
2022-08-03 12:58:25 +0800

    fix: unstake input valid

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -320,6 +320,13 @@ export const ButtonFarm = styled(Base)`
   background: linear-gradient(241deg, #FF978E 0%, #FB548B 100%);
   font-size: 20px;
   font-weight: 500;
+
+  &:disabled {
+    opacity: 50%;
+    cursor: auto;
+    box-shadow: none;
+    background-color: ${({ theme }) => theme.red1};
+  }
 `
 
 export const ButtonBorder = styled(Base)`
