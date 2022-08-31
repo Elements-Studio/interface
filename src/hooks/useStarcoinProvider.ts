80a05b7df9f34bf99c0e38b589fa277327a89c96
montecristo43420 <montecristo43420@gmail.com>
2022-08-31 12:47:27 +0800

    useStarcoinProvider should listen on provider changing

--- src/hooks/useStarcoinProvider.ts
+++ src/hooks/useStarcoinProvider.ts
@@ -6,17 +6,18 @@ import { useActiveWeb3React } from './web3'
 
 export function useStarcoinProvider(): providers.Web3Provider | providers.JsonRpcProvider {
   const { chainId } = useActiveWeb3React()
+  const provider = window.starcoin ? window.starcoin : window.obstarcoin
   return useMemo(() => {
     try {
       const provider = window.starcoin ? window.starcoin : window.obstarcoin
-      console.log('useStarcoinProvider', 'window.starcoin=', window.starcoin, 'window.obstarcoin=', window.obstarcoin, { provider })
+      console.log('11 useStarcoinProvider', 'window.starcoin=', window.starcoin, 'window.obstarcoin=', window.obstarcoin, { provider })
       // We must specify the network as 'any' for starcoin to allow network changes
       return new providers.Web3Provider(provider!, 'any')
     } catch {
       console.error('providers.Web3Provider is not ok, use providers.JsonRpcProvider')
       return new providers.JsonRpcProvider(NETWORK_URLS[chainId as SupportedChainId])
     }
-  }, [chainId])
+  }, [chainId, provider])
 }
 
 export function useStarcoinJsonRPCProvider(): providers.JsonRpcProvider {
