384e8f0af3c21bf6aecb1705cbbac0018d308e25
montecristo43420 <montecristo43420@gmail.com>
2022-10-26 02:29:22 +0800

    Support APT while add/remove liquidity

--- src/utils/unwrappedToken.ts
+++ src/utils/unwrappedToken.ts
@@ -1,11 +1,13 @@
 import { Currency } from '@uniswap/sdk-core'
-import { ExtendedStar, WETH9_EXTENDED } from '../constants/tokens'
+import { ExtendedStar, ExtendedApt, WETH9_EXTENDED } from '../constants/tokens'
 import { supportedChainId } from './supportedChainId'
+import getNetworkType from './getNetworkType'
 
 export function unwrappedToken(currency: Currency): Currency {
   if (currency.isNative) return currency
   const formattedChainId = supportedChainId(currency.chainId)
+  const networkType = getNetworkType()
   if (formattedChainId && WETH9_EXTENDED[formattedChainId] && currency.equals(WETH9_EXTENDED[formattedChainId]))
-    return ExtendedStar.onChain(currency.chainId)
+    return networkType === 'STARCOIN' ? ExtendedStar.onChain(currency.chainId) : ExtendedApt.onChain(currency.chainId)
   return currency
 }
