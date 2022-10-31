2ea2bef6d254ea191dfe1a74d9cb4cdf8856997d
montecristo43420 <montecristo43420@gmail.com>
2022-11-01 01:25:41 +0800

    Tips support aptos

--- src/components/WalletModal/index.tsx
+++ src/components/WalletModal/index.tsx
@@ -315,7 +315,7 @@ export default function WalletModal({
           <ContentWrapper>
             {error instanceof UnsupportedChainIdError ? (
               <h5>
-                <Trans>Please connect to the appropriate Starcoin network.</Trans>
+                <Trans>Please connect to the appropriate Starcoin/Aptos network.</Trans>
               </h5>
             ) : (
               <Trans>Error connecting. Try refreshing the page.</Trans>
