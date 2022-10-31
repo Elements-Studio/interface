0de9d4afc75d00d3ba403cc6977c478ff73aed87
montecristo43420 <montecristo43420@gmail.com>
2022-10-31 22:13:10 +0800

    useSelectChain is ok, add networkType in wallet_switchStarcoinChain

--- src/hooks/useSelectNetWork.ts
+++ src/hooks/useSelectNetWork.ts
@@ -1,19 +1,31 @@
 import { useCallback } from 'react'
 import { useAppDispatch } from 'state/hooks'
+import { useActiveWeb3React } from 'hooks/web3'
+// import { UnsupportedChainIdError, useWeb3React } from '@starcoin/starswap-web3-core'
 // import { switchChain } from 'utils/switchChain'
 import { useGetType, useSetType } from 'state/networktype/hooks'
+import { switchToNetwork } from 'utils/switchToNetwork'
 
 export default function useSelectChain() {
   const dispatch = useAppDispatch()
   const networkType = useGetType();
   const setType = useSetType();
-
+  const { chainId, library } = useActiveWeb3React()
   return useCallback(
     async (targetChain: string) => {
       if (!networkType) { return }
+      if (!library?.provider?.request || !chainId) {
+        return
+      }
 
       try {
         setType(targetChain);
+
+        const targetChainId = targetChain === 'STARCOIN' ? 1 : 2
+        await switchToNetwork({ library, chainId: targetChainId, networkType: targetChain })
+          .then((x) => console.log({ x }))
+          .catch((error) => console.log({ error }))
+
         // await switchChain(connector, targetChain)
       } catch (error) {
         console.error('Failed to switch networks', error)
@@ -22,6 +34,6 @@ export default function useSelectChain() {
         // dispatch(addPopup({ content: { failedSwitchNetwork: targetChain }, key: `failed-network-switch` }))
       }
     },
-    [networkType, setType]
+    [networkType, setType, chainId, library]
   )
 }
--- src/utils/switchToNetwork.ts
+++ src/utils/switchToNetwork.ts
@@ -5,17 +5,18 @@ import { Web3Provider } from '@starcoin/providers'
 interface SwitchNetworkArguments {
   library: Web3Provider
   chainId: SupportedChainId
+  networkType?: string
 }
 
 // provider.request returns Promise<any>, but wallet_switchEthereumChain must return null or throw
 // see https://github.com/rekmarks/EIPs/blob/3326-create/EIPS/eip-3326.md for more info on wallet_switchEthereumChain
-export async function switchToNetwork({ library, chainId }: SwitchNetworkArguments): Promise<null | void> {
+export async function switchToNetwork({ library, chainId, networkType = 'STARCOIN' }: SwitchNetworkArguments): Promise<null | void> {
   if (!library?.provider?.request) {
     return
   }
   const formattedChainId = utils.hexStripZeros(BigNumber.from(chainId).toHexString())
   return library?.provider.request({
-    method: 'wallet_switchEthereumChain',
-    params: [{ chainId: formattedChainId }],
+    method: 'wallet_switchStarcoinChain',
+    params: [{ chainId: formattedChainId, networkType }],
   })
 }
