07cc1099e3d3acf2da9f4be0f59bce3fff4a1b18
montecristo43420 <montecristo43420@gmail.com>
2022-11-02 00:22:19 +0800

    fix farmingTvlInUsd in Farm title in aptos mainnet

--- src/components/farm/FarmTitle.tsx
+++ src/components/farm/FarmTitle.tsx
@@ -4,7 +4,7 @@ import ReactGA from 'react-ga'
 import styled from 'styled-components/macro'
 import { useActiveWeb3React } from 'hooks/web3'
 import { StyledInternalLink, TYPE } from '../../theme'
-
+import { useGetCurrentNetwork } from 'state/networktype/hooks'
 import axios from 'axios';
 import useSWR from "swr";
 
@@ -28,16 +28,9 @@ const TitleTotal = styled.div<{ margin?: string; maxWidth?: string }>`
 `
 
 export default function FarmTitle() {
-    let network = 'barnard';
     const { chainId } = useActiveWeb3React()
-    if (chainId === 1) {
-      network = 'main';
-    }
-    if (chainId === 252) {
-      network = 'proxima';
-    }
 
-    const lpTokenScalingFactor = 1000000000;
+    const network = useGetCurrentNetwork(chainId)
 
     const { data, error } = useSWR(
       // "http://a1277180fcb764735801852ac3de308f-21096515.ap-northeast-1.elb.amazonaws.com:80/v1/starswap/farmingTvlInUsd",
