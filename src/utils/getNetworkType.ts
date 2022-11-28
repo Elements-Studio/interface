d78bb952cb61ef3175d8e968b7b3c54331eaf3c9
montecristo43420 <montecristo43420@gmail.com>
2022-11-28 18:06:27 +0800

    fix: window.starcoin is not exists while using other aptos wallets

--- src/utils/getNetworkType.ts
+++ src/utils/getNetworkType.ts
@@ -1,19 +1,20 @@
 import { SupportedChainId_STARCOIN, SupportedChainId_APTOS } from 'constants/chains'
 import { BigNumber } from 'ethers'
 export default function getNetworkType() {
-  const chainId = window.starcoin?.chainId
-  let networkType = ''
-  // the mainnet chainId of both Aptos and Starcoin is the same: 0x1
-  // we can determine it by selectedAddress length
-  if (chainId === '0x1') {
-    networkType = window.starcoin?.selectedAddress?.length === 66 ? 'APTOS' : 'STARCOIN'
-  } else {
-    const networkVersion = BigNumber.from(chainId).toNumber()
-    if (networkVersion in SupportedChainId_STARCOIN) {
-      networkType = 'STARCOIN'
-    } else if (networkVersion in SupportedChainId_APTOS) {
-      networkType = 'APTOS'
-    }
-  }
-  return networkType
+  return 'APTOS'
+  // const chainId = window.starcoin?.chainId
+  // let networkType = ''
+  // // the mainnet chainId of both Aptos and Starcoin is the same: 0x1
+  // // we can determine it by selectedAddress length
+  // if (chainId === '0x1') {
+  //   networkType = window.starcoin?.selectedAddress?.length === 66 ? 'APTOS' : 'STARCOIN'
+  // } else {
+  //   const networkVersion = BigNumber.from(chainId).toNumber()
+  //   if (networkVersion in SupportedChainId_STARCOIN) {
+  //     networkType = 'STARCOIN'
+  //   } else if (networkVersion in SupportedChainId_APTOS) {
+  //     networkType = 'APTOS'
+  //   }
+  // }
+  // return networkType
 }
